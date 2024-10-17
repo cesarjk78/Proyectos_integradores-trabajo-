1811beb385e932cf3050279a23eebfe7941ede0d
FabioOC01 <ochoacuencafabio@gmail.com>
2024-10-17 00:21:26 -0500

    Login y registro +template base

--- proyectos/urls.py
+++ proyectos/urls.py
@@ -4,4 +4,6 @@ from . import views  # Importar las vistas desde el archivo views.py
 urlpatterns = [
     path('', views.home, name='home'),  # Ruta para la vista principal de la app
     path('quienes-somos/', views.quienes_somos, name='quienes_somos'),
+     path('iniciar-sesion/', views.iniciar_sesion, name='iniciar_sesion'),
+         path('registrar/', views.crear_usuario, name='registrar'), 
 ]
--- proyectos/views.py
+++ proyectos/views.py
@@ -1,7 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 def home(request):
     return render(request, 'proyectos/index.html')  # Asegúrate de que la ruta coincida con la ubicación de tu archivo
 
 def quienes_somos(request):
     return render(request, 'proyectos/quienesomos.html') 
+
+#Login
+
+def es_admin(user):
+    return user.groups.filter(name='Admin').exists()
+
+def crear_usuario(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+       
+        if User.objects.filter(email=email).exists():
+            return redirect('registrar')
+        else:
+         
+            user = User.objects.create_user(
+                username=email,  
+                email=email,
+                password=password,
+                first_name=first_name,
+                last_name=last_name
+            )
+            user.save()
+            return redirect('iniciar_sesion')  
+
+    return render(request, 'proyectos/registrar.html')
+
+def iniciar_sesion(request):
+    if request.method == 'POST':
+        email = request.POST.get('email') 
+        password = request.POST.get('password')
+
+        try:
+        
+            user = User.objects.get(email=email)
+            user = authenticate(request, username=user.username, password=password)
+        except User.DoesNotExist:
+            user = None
+
+        if user is not None:
+            login(request, user)
+            return redirect('http://127.0.0.1:8000/')
+        else:
+            messages.error(request, 'Correo o contraseña incorrectos.')
+
+    return render(request, 'proyectos/iniciar_sesion.html')
+
+def cerrar_sesion(request):
+    logout(request)
+    return redirect('proyectos/iniciar_sesion.html')
